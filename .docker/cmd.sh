bdc84f39c1afc61cd8e9bc23cb815ca061db358c
Vincent Composieux <vincent@composieux.fr>
2023-01-17 13:19:25 +0100

    Fixed docker images to be unprivileged (non-root)

--- .docker/cmd.sh
+++ .docker/cmd.sh
@@ -1,7 +1,14 @@
 #!/bin/sh
 
 echo "running nginx"
-nginx
+nginx -g "daemon off;" &
+NGINX_PID="$!"
+
 
 echo "running backend"
-/authz
+/authz &
+AUTHZ_PID="$!"
+
+trap "kill $NGINX_PIDC $AUTHZ_PID" exit INT TERM
+
+wait
